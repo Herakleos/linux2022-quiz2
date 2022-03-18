7fb6e9e1e85d275ef345bf6cc991daa70f5a885d
John Chen <chunichen@protonmail.com>
2022-03-18 10:08:21 +0800

    Modify format

--- test.c
+++ test.c
@@ -11,16 +11,15 @@ int main()
 {
     srand((unsigned int) time(NULL));
     int a = rand() % 1000, b = rand() % 500;
-    printf("--- Test case ---\na: %d, b: %d\n\n", a, b);
+    printf("\tTest case\t\na: %d, b: %d\n\n", a, b);
 
-    puts("--- EXP 1 ~ 3 ---");
-    printf("Average: %u\n", average(a, b));     // EXP1
-    printf("Average: %u\n\n", average2(a, b));  // EXP2, EXP3
+    puts("\tEXP 1 ~ 3\t");
+    printf("Average: %u\n", average(a, b));             // EXP1
+    printf("Average: %u\n\n", average2(a, b));          // EXP2, EXP3
 
-    puts("--- EXP 4 ~ 5 ---");
-    printf("Min: %u\n", min(a, b));
-    printf("Min2: %u\n", min2(a, b));
-    printf("Max: %u\n\n", max(a, b));           // EXP4, EXP5
+    puts("\tEXP 4 ~ 5\t");
+    printf("Min: %u\tMin2: %u\n", min(a, b), min2(a, b));
+    printf("Max: %u\n\n", max(a, b));                   // EXP4, EXP5
 
     return 0;
 }
